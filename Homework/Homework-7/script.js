cc4ad26113ac124ffbcabb0305163f3d440fc75c
MounirHader <2527137@student.vu.nl>
2015-12-11 22:39:16 +0100

    Homework-7

---
+++ Homework/Homework-7/script.js
@@ -0,0 +1,447 @@
+country_codes = [
+    ["af", "AFG", "Afghanistan"],
+    ["ax", "ALA", "Åland Islands"],
+    ["al", "ALB", "Albania"],
+    ["dz", "DZA", "Algeria"],
+    ["as", "ASM", "American Samoa"],
+    ["ad", "AND", "Andorra"],
+    ["ao", "AGO", "Angola"],
+    ["ai", "AIA", "Anguilla"],
+    ["aq", "ATA", "Antarctica"],
+    ["ag", "ATG", "Antigua and Barbuda"],
+    ["ar", "ARG", "Argentina"],
+    ["am", "ARM", "Armenia"],
+    ["aw", "ABW", "Aruba"],
+    ["au", "AUS", "Australia"],
+    ["at", "AUT", "Austria"],
+    ["az", "AZE", "Azerbaijan"],
+    ["bs", "BHS", "Bahamas"],
+    ["bh", "BHR", "Bahrain"],
+    ["bd", "BGD", "Bangladesh"],
+    ["bb", "BRB", "Barbados"],
+    ["by", "BLR", "Belarus"],
+    ["be", "BEL", "Belgium"],
+    ["bz", "BLZ", "Belize"],
+    ["bj", "BEN", "Benin"],
+    ["bm", "BMU", "Bermuda"],
+    ["bt", "BTN", "Bhutan"],
+    ["bo", "BOL", "Bolivia"],
+    ["bq", "BES", "Bonaire, Sint Eustatius and Saba"],
+    ["ba", "BIH", "Bosnia and Herzegovina"],
+    ["bw", "BWA", "Botswana"],
+    ["bv", "BVT", "Bouvet Island"],
+    ["br", "BRA", "Brazil"],
+    ["io", "IOT", "British Indian Ocean Territory"],
+    ["bn", "BRN", "Brunei Darussalam"],
+    ["bg", "BGR", "Bulgaria"],
+    ["bf", "BFA", "Burkina Faso"],
+    ["bi", "BDI", "Burundi"],
+    ["kh", "KHM", "Cambodia"],
+    ["cm", "CMR", "Cameroon"],
+    ["ca", "CAN", "Canada"],
+    ["cv", "CPV", "Cape Verde"],
+    ["ky", "CYM", "Cayman Islands"],
+    ["cf", "CAF", "Central African Republic"],
+    ["td", "TCD", "Chad"],
+    ["cl", "CHL", "Chile"],
+    ["cn", "CHN", "China"],
+    ["cx", "CXR", "Christmas Island"],
+    ["cc", "CCK", "Cocos (Keeling) Islands"],
+    ["co", "COL", "Colombia"],
+    ["km", "COM", "Comoros"],
+    ["cg", "COG", "Republic of the Congo"],
+    ["cd", "COD", "Democratic Republic of the Congo"],
+    ["ck", "COK", "Cook Islands"],
+    ["cr", "CRI", "Costa Rica"],
+    ["ci", "CIV", "Ivory Coast"],
+    ["hr", "HRV", "Croatia"],
+    ["cu", "CUB", "Cuba"],
+    ["cw", "CUW", "Curaçao"],
+    ["cy", "CYP", "Cyprus"],
+    ["cz", "CZE", "Czech Republic"],
+    ["dk", "DNK", "Denmark"],
+    ["dj", "DJI", "Djibouti"],
+    ["dm", "DMA", "Dominica"],
+    ["do", "DOM", "Dominican Republic"],
+    ["ec", "ECU", "Ecuador"],
+    ["eg", "EGY", "Egypt"],
+    ["sv", "SLV", "El Salvador"],
+    ["gq", "GNQ", "Equatorial Guinea"],
+    ["er", "ERI", "Eritrea"],
+    ["ee", "EST", "Estonia"],
+    ["et", "ETH", "Ethiopia"],
+    ["fk", "FLK", "Falkland Islands (Malvinas)"],
+    ["fo", "FRO", "Faroe Islands"],
+    ["fj", "FJI", "Fiji"],
+    ["fi", "FIN", "Finland"],
+    ["fr", "FRA", "France"],
+    ["gf", "GUF", "French Guiana"],
+    ["pf", "PYF", "French Polynesia"],
+    ["tf", "ATF", "French Southern Territories"],
+    ["ga", "GAB", "Gabon"],
+    ["gm", "GMB", "Gambia"],
+    ["ge", "GEO", "Georgia"],
+    ["de", "DEU", "Germany"],
+    ["gh", "GHA", "Ghana"],
+    ["gi", "GIB", "Gibraltar"],
+    ["gr", "GRC", "Greece"],
+    ["gl", "GRL", "Greenland"],
+    ["gd", "GRD", "Grenada"],
+    ["gp", "GLP", "Guadeloupe"],
+    ["gu", "GUM", "Guam"],
+    ["gt", "GTM", "Guatemala"],
+    ["gg", "GGY", "Guernsey"],
+    ["gn", "GIN", "Guinea"],
+    ["gw", "GNB", "Guinea-Bissau"],
+    ["gy", "GUY", "Guyana"],
+    ["ht", "HTI", "Haiti"],
+    ["hm", "HMD", "Heard Island and McDonald Islands"],
+    ["va", "VAT", "Holy See (Vatican City State)"],
+    ["hn", "HND", "Honduras"],
+    ["hk", "HKG", "Hong Kong"],
+    ["hu", "HUN", "Hungary"],
+    ["is", "ISL", "Iceland"],
+    ["in", "IND", "India"],
+    ["id", "IDN", "Indonesia"],
+    ["ir", "IRN", "Iran"],
+    ["iq", "IRQ", "Iraq"],
+    ["ie", "IRL", "Ireland"],
+    ["im", "IMN", "Isle of Man"],
+    ["il", "ISR", "Israel"],
+    ["it", "ITA", "Italy"],
+    ["jm", "JAM", "Jamaica"],
+    ["jp", "JPN", "Japan"],
+    ["je", "JEY", "Jersey"],
+    ["jo", "JOR", "Jordan"],
+    ["kz", "KAZ", "Kazakhstan"],
+    ["ke", "KEN", "Kenya"],
+    ["ki", "KIR", "Kiribati"],
+    ["kp", "PRK", "North Korea"],
+    ["kr", "KOR", "South Korea"],
+    ["kw", "KWT", "Kuwait"],
+    ["kg", "KGZ", "Kyrgyzstan"],
+    ["la", "LAO", "Laos"],
+    ["lv", "LVA", "Latvia"],
+    ["lb", "LBN", "Lebanon"],
+    ["ls", "LSO", "Lesotho"],
+    ["lr", "LBR", "Liberia"],
+    ["ly", "LBY", "Libya"],
+    ["li", "LIE", "Liechtenstein"],
+    ["lt", "LTU", "Lithuania"],
+    ["lu", "LUX", "Luxembourg"],
+    ["mo", "MAC", "Macao"],
+    ["mk", "MKD", "Macedonia"],
+    ["mg", "MDG", "Madagascar"],
+    ["mw", "MWI", "Malawi"],
+    ["my", "MYS", "Malaysia"],
+    ["mv", "MDV", "Maldives"],
+    ["ml", "MLI", "Mali"],
+    ["mt", "MLT", "Malta"],
+    ["mh", "MHL", "Marshall Islands"],
+    ["mq", "MTQ", "Martinique"],
+    ["mr", "MRT", "Mauritania"],
+    ["mu", "MUS", "Mauritius"],
+    ["yt", "MYT", "Mayotte"],
+    ["mx", "MEX", "Mexico"],
+    ["fm", "FSM", "Micronesia, Federated States of"],
+    ["md", "MDA", "Moldova"],
+    ["mc", "MCO", "Monaco"],
+    ["mn", "MNG", "Mongolia"],
+    ["me", "MNE", "Montenegro"],
+    ["ms", "MSR", "Montserrat"],
+    ["ma", "MAR", "Morocco"],
+    ["mz", "MOZ", "Mozambique"],
+    ["mm", "MMR", "Myanmar"],
+    ["na", "NAM", "Namibia"],
+    ["nr", "NRU", "Nauru"],
+    ["np", "NPL", "Nepal"],
+    ["nl", "NLD", "Netherlands"],
+    ["nc", "NCL", "New Caledonia"],
+    ["nz", "NZL", "New Zealand"],
+    ["ni", "NIC", "Nicaragua"],
+    ["ne", "NER", "Niger"],
+    ["ng", "NGA", "Nigeria"],
+    ["nu", "NIU", "Niue"],
+    ["nf", "NFK", "Norfolk Island"],
+    ["mp", "MNP", "Northern Mariana Islands"],
+    ["no", "NOR", "Norway"],
+    ["om", "OMN", "Oman"],
+    ["pk", "PAK", "Pakistan"],
+    ["pw", "PLW", "Palau"],
+    ["ps", "PSE", "Palestine, State of"],
+    ["pa", "PAN", "Panama"],
+    ["pg", "PNG", "Papua New Guinea"],
+    ["py", "PRY", "Paraguay"],
+    ["pe", "PER", "Peru"],
+    ["ph", "PHL", "Philippines"],
+    ["pn", "PCN", "Pitcairn"],
+    ["pl", "POL", "Poland"],
+    ["pt", "PRT", "Portugal"],
+    ["pr", "PRI", "Puerto Rico"],
+    ["qa", "QAT", "Qatar"],
+    ["re", "REU", "Réunion"],
+    ["ro", "ROU", "Romania"],
+    ["ru", "RUS", "Russia"],
+    ["rw", "RWA", "Rwanda"],
+    ["bl", "BLM", "Saint Barthélemy"],
+    ["sh", "SHN", "Saint Helena, Ascension and Tristan da Cunha"],
+    ["kn", "KNA", "Saint Kitts and Nevis"],
+    ["lc", "LCA", "Saint Lucia"],
+    ["mf", "MAF", "Saint Martin (French part)"],
+    ["pm", "SPM", "Saint Pierre and Miquelon"],
+    ["vc", "VCT", "Saint Vincent and the Grenadines"],
+    ["ws", "WSM", "Samoa"],
+    ["sm", "SMR", "San Marino"],
+    ["st", "STP", "Sao Tome and Principe"],
+    ["sa", "SAU", "Saudi Arabia"],
+    ["sn", "SEN", "Senegal"],
+    ["rs", "SRB", "Serbia"],
+    ["sc", "SYC", "Seychelles"],
+    ["sl", "SLE", "Sierra Leone"],
+    ["sg", "SGP", "Singapore"],
+    ["sx", "SXM", "Sint Maarten (Dutch part)"],
+    ["sk", "SVK", "Slovakia"],
+    ["si", "SVN", "Slovenia"],
+    ["sb", "SLB", "Solomon Islands"],
+    ["so", "SOM", "Somalia"],
+    ["za", "ZAF", "South Africa"],
+    ["gs", "SGS", "South Georgia and the South Sandwich Islands"],
+    ["ss", "SSD", "South Sudan"],
+    ["es", "ESP", "Spain"],
+    ["lk", "LKA", "Sri Lanka"],
+    ["sd", "SDN", "Sudan"],
+    ["sr", "SUR", "Suriname"],
+    ["sj", "SJM", "Svalbard and Jan Mayen"],
+    ["sz", "SWZ", "Swaziland"],
+    ["se", "SWE", "Sweden"],
+    ["ch", "CHE", "Switzerland"],
+    ["sy", "SYR", "Syria"],
+    ["tw", "TWN", "Taiwan, Province of China"],
+    ["tj", "TJK", "Tajikistan"],
+    ["tz", "TZA", "Tanzania"],
+    ["th", "THA", "Thailand"],
+    ["tl", "TLS", "Timor-Leste"],
+    ["tg", "TGO", "Togo"],
+    ["tk", "TKL", "Tokelau"],
+    ["to", "TON", "Tonga"],
+    ["tt", "TTO", "Trinidad and Tobago"],
+    ["tn", "TUN", "Tunisia"],
+    ["tr", "TUR", "Turkey"],
+    ["tm", "TKM", "Turkmenistan"],
+    ["tc", "TCA", "Turks and Caicos Islands"],
+    ["tv", "TUV", "Tuvalu"],
+    ["ug", "UGA", "Uganda"],
+    ["ua", "UKR", "Ukraine"],
+    ["ae", "ARE", "United Arab Emirates"],
+    ["gb", "GBR", "United Kingdom"],
+    ["us", "USA", "United States"],
+    ["um", "UMI", "United States Minor Outlying Islands"],
+    ["uy", "URY", "Uruguay"],
+    ["uz", "UZB", "Uzbekistan"],
+    ["vu", "VUT", "Vanuatu"],
+    ["ve", "VEN", "Venezuela"],
+    ["vn", "VNM", "Vietnam"],
+    ["vg", "VGB", "Virgin Islands, British"],
+    ["vi", "VIR", "Virgin Islands, U.S."],
+    ["wf", "WLF", "Wallis and Futuna"],
+    ["eh", "ESH", "Western Sahara"],
+    ["ye", "YEM", "Yemen"],
+    ["zm", "ZMB", "Zambia"],
+    ["zw", "ZWE", "Zimbabwe"] ];
+
+// get data
+rawdata = document.getElementById('rawdata').value;
+var jsondata = JSON.parse(rawdata);
+
+colour_keys = [];
+code_list = []
+consumption_list = []
+beer_list = []
+wine_list = []
+spirits_list = []
+other_list = []
+country_list = []
+
+// loop over dataset
+for (i = 0, j = jsondata.length; i < j; i++){
+  // country of interest
+  target_country = jsondata[i][0];
+  target_country_consumption = jsondata[i][1];
+
+  // find id and set fillKey
+  for (k = 0, l = country_codes.length; k < l; k++){
+    if (country_codes[k][2] == target_country){
+      land_id = country_codes[k][1];
+      code_list.push(land_id);
+      consumption_list.push(target_country_consumption)
+      beer_list.push(jsondata[i][4])
+      wine_list.push(jsondata[i][5])
+      spirits_list.push(jsondata[i][6])
+      other_list.push(jsondata[i][7])
+      country_list.push(target_country)
+
+      if (target_country_consumption > 15 && target_country_consumption <= 18) {
+        fillKey = "15+ "
+        colour_keys[land_id] = {fillKey};
+      }
+
+      else if (target_country_consumption > 10 && target_country_consumption <= 15) {
+        fillKey = "10-15 "
+        colour_keys[land_id] = {fillKey};
+      }
+
+      else if (target_country_consumption > 5 && target_country_consumption <= 10) {
+        fillKey = "5-10 "
+        colour_keys[land_id] = {fillKey};
+      }
+
+      else if (target_country_consumption <= 5){
+        fillKey = "0-5 "
+        colour_keys[land_id] = {fillKey};
+      }
+    }}
+  }
+
+// function that draws pie chart for clicked click_country
+pie_chart = function(click_country){
+  for (var i = 0, j = code_list.length; i < j; i++){
+    if (click_country == code_list[i]){
+			datah = [country_list[i], beer_list[i], wine_list[i], spirits_list[i], other_list[i], consumption_list[i]];
+
+		var pie = new d3pie("pieChart", {
+			"header": {
+				"title": {
+					"text": "People in " + datah[0] + " drink " + datah[5] + " Liters of pure alcohol per year",
+					"fontSize": 30,
+          "fontWeight": 900,
+          "font":"Palatino",
+          "font-weight": "1000px"
+				},
+				"subtitle": {
+					"text": "This chart shows which drinks make up those liters",
+					"color": "#999999",
+					"fontSize": 20,
+					"font": "open sans"
+				},
+				"titleSubtitlePadding": 6
+			},
+			"size": {
+				"canvasWidth": 1400,
+				"pieInnerRadius": "15%",
+				"pieOuterRadius": "100%"
+			},
+			"data": {
+				"sortOrder": "value-desc",
+				"content": [
+					{
+						"label": "Wine",
+						"value": datah[2],
+						"color": "#961919"
+					},
+					{
+						"label": "Spirits",
+						"value": datah[3],
+						"color": "#8856a7"
+					},
+					{
+						"label": "Beer",
+						"value": datah[1],
+						"color": "#FBB117"
+					},
+					{
+						"label": "Other",
+						"value": datah[4],
+						"color": "grey"
+					},
+					{
+						"label": "",
+						"value": null,
+						"color": "#efefef"
+					}
+				]
+			},
+			"labels": {
+				"outer": {
+					"pieDistance": 32
+				},
+				"inner": {
+					"hideWhenLessThanPercentage": 5
+				},
+				"mainLabel": {
+					"font": "open sans",
+					"fontSize": 12
+				},
+				"percentage": {
+					"color": "#ffffff",
+					"fontSize": 19,
+					"decimalPlaces": 0
+				},
+				"value": {
+					"color": "#adadad",
+					"fontSize": 11
+				},
+				"lines": {
+					"enabled": true
+				},
+				"truncation": {
+					"enabled": true
+				}
+			},
+			"effects": {
+				"pullOutSegmentOnClick": {
+					"effect": "elastic",
+					"speed": 400,
+					"size": 15
+				}
+			},
+			"misc": {
+				"gradient": {
+					"enabled": true,
+					"percentage": 100
+				}
+			},
+			"callbacks": {}
+		});
+	}}};
+
+// scroll down function
+scroll_down = function () {
+     $('html, body').animate({
+     scrollTop:500
+   }, 'fast');
+   return false;
+   }
+
+// remove old piechart function
+remove_old_chart = function() {
+    // select all barscharts
+    var parent = d3.select("#pieChart")
+    parent.selectAll("*").remove()
+	  }
+
+// create data map with value-coloured countries
+var map = new Datamap({
+  element: document.getElementById("container"),
+
+  fills: {
+          '15+ ': '#99000d',
+          '10-15 ': '#ef3b2c',
+          '5-10 ': '#fc9272',
+          '0-5 ': '#fee0d2',
+          'NO DATA': 'black',
+      },
+
+  data: colour_keys,
+
+  // on click function
+  done: function(map) {
+         map.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+             click_country = geography.id;
+             remove_old_chart();
+             pie_chart(click_country);
+             scroll_down();
+       });
+     }
+});
+
+map.legend({legendTitle: "Liters of pure alcohol per capita per year"});
